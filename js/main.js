7c60d81789d9b20b432b8a59434c6a856ec65482
Sebrinna <157453833+Sebrinna@users.noreply.github.com>
2024-02-13 02:55:26 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+$('.sliders').slick({
+  autoplay: true,
+  autoplaySpeed: 3000,
+  arrows: false,
+  pauseOnHover: true,
+  playOnHover: true,
+  swipe: true,
+});
+
+
+
+$('.buy').mouseon(() => {
+  $('.popUp').css('display','block')
+}
+)
+$('.inner-text').css('color','red');
+
+
---
+++ js/purchase.js
@@ -0,0 +1,9 @@
+
+$('.buy').click(
+    function () {
+        $('.popUp').fadeIn(2000);
+})
+
+$('.container').click(function(){
+    $('.popUp').fadeOut(2000);
+})
